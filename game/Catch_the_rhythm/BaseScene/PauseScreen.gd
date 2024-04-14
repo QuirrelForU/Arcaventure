3ad6b58ef278847a0f6e7b2e2d0a9d5eb66b68ed
QuirrelForU <symmlux@gmail.com>
2024-04-14 16:52:56 +0700

    volume slider in rhythm

--- game/Catch_the_rhythm/BaseScene/PauseScreen.gd
+++ game/Catch_the_rhythm/BaseScene/PauseScreen.gd
@@ -5,6 +5,10 @@ var is_paused = false setget set_is_paused
 onready var stats = $"../../Stats"
 
 
+
+func _ready():
+	$CenterContainer/VBoxContainer/VolumeSlider.value = Globals.volume
+
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
 		self.is_paused = !is_paused
@@ -33,3 +37,7 @@ func _on_MenuButton_pressed():
 	self.is_paused = false
 	ChangeScene.change_scene("res://BaseMenu/BaseMenu.tscn")
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _on_VolumeSlider_value_changed(value):
+	Globals.volume = value
