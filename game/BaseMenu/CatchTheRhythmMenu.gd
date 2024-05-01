9ed94dcab05b10dbd3cb3674a7442ddc71253697
QuirrelForU <symmlux@gmail.com>
2024-05-02 00:01:52 +0700

    buttons for new level

--- game/BaseMenu/CatchTheRhythmMenu.gd
+++ game/BaseMenu/CatchTheRhythmMenu.gd
@@ -2,12 +2,12 @@ extends Control
 
 
 onready var game_setting_saver = $"../GameSettingsSaver"
-
+onready var difficulties = $VBoxContainer/Difficulties
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/HBoxContainer/VBoxContainer/Difficulties.add_item("Normal")
-	$VBoxContainer/HBoxContainer/VBoxContainer/Difficulties.add_item("Easy")
-	$VBoxContainer/HBoxContainer/VBoxContainer/Difficulties.add_item("Hard")
+	difficulties.add_item("Normal")
+	difficulties.add_item("Easy")
+	difficulties.add_item("Hard")
 
 
 
@@ -16,22 +16,41 @@ func _on_StartCatchTheRhythm1_pressed():
 	ChangeScene.change_scene("res://Catch_the_rhythm/BaseScene/BaseRhythm.tscn")
 
 
-func _on_StartCatchTheRhythm2_pressed():
-	print("easy")
-
-
-func _on_StartCatchTheRhythm3_pressed():
-	print("hard")
-
 
 func _on_Difficulties_item_selected(index):
-	var difficulty = $VBoxContainer/HBoxContainer/VBoxContainer/Difficulties.get_item_text(index)
+	var difficulty = difficulties.get_item_text(index)
 	$VBoxContainer/HBoxContainer/StartCatchTheRhythm1.visible = false
 	$VBoxContainer/HBoxContainer/StartCatchTheRhythmEasy.visible = false
 	$VBoxContainer/HBoxContainer/StartCatchTheRhythmHard.visible = false
+	$VBoxContainer/HBoxContainer3/StartCatchTheRhythm2.visible = false
+	$VBoxContainer/HBoxContainer3/StartCatchTheRhythmEasy2.visible = false
+	$VBoxContainer/HBoxContainer3/StartCatchTheRhythmHard2.visible = false
 	if difficulty == "Normal":
 		$VBoxContainer/HBoxContainer/StartCatchTheRhythm1.visible = true
+		$VBoxContainer/HBoxContainer3/StartCatchTheRhythm2.visible = true
 	elif difficulty == "Easy":
 		$VBoxContainer/HBoxContainer/StartCatchTheRhythmEasy.visible = true
+		$VBoxContainer/HBoxContainer3/StartCatchTheRhythmEasy2.visible = true
 	elif difficulty == "Hard":
 		$VBoxContainer/HBoxContainer/StartCatchTheRhythmHard.visible = true
+		$VBoxContainer/HBoxContainer3/StartCatchTheRhythmHard2.visible = true
+
+
+func _on_StartCatchTheRhythmEasy_pressed():
+	pass # Replace with function body.
+
+
+func _on_StartCatchTheRhythmHard_pressed():
+	pass # Replace with function body.
+
+
+func _on_StartCatchTheRhythm2_pressed():
+	pass # Replace with function body.
+
+
+func _on_StartCatchTheRhythmEasy2_pressed():
+	pass # Replace with function body.
+
+
+func _on_StartCatchTheRhythmHard2_pressed():
+	pass # Replace with function body.
--- game/BaseMenu/PlayerNameInput.gd
+++ game/BaseMenu/PlayerNameInput.gd
@@ -7,7 +7,13 @@ func _on_PlayerName_text_changed(new_text):
 		$"../HBoxContainer/StartCatchTheRhythm1".disabled = true
 		$"../HBoxContainer/StartCatchTheRhythmEasy".disabled = true
 		$"../HBoxContainer/StartCatchTheRhythmHard".disabled = true
+		$"../HBoxContainer3/StartCatchTheRhythm2".disabled = true
+		$"../HBoxContainer3/StartCatchTheRhythmEasy2".disabled = true
+		$"../HBoxContainer3/StartCatchTheRhythmHard2".disabled = true
 	else:
 		$"../HBoxContainer/StartCatchTheRhythm1".disabled = false
 		$"../HBoxContainer/StartCatchTheRhythmEasy".disabled = false
 		$"../HBoxContainer/StartCatchTheRhythmHard".disabled = false
+		$"../HBoxContainer3/StartCatchTheRhythm2".disabled = false
+		$"../HBoxContainer3/StartCatchTheRhythmEasy2".disable = false
+		$"../HBoxContainer3/StartCatchTheRhythmHard2".disabled = false
